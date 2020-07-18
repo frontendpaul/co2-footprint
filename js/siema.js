7a52f5e0cc51250f23e506f6bf1402715f0a141b
frontendpaul <matizbrave@gmail.com>
2020-07-18 14:37:02 +0200

    first draft of bottom nav done

--- js/siema.js
+++ js/siema.js
@@ -1,14 +1,25 @@
-const mySiema = new Siema({
+class MySiema extends Siema {
+  handleBottomNav() {    
+    const addOrRemove = this.currentSlide > 1 ? "add" : "remove";
+
+    document.querySelector(".bottom-nav").classList[addOrRemove]("is-visible");
+  }
+}
+
+const mySiemaInstance = new MySiema({
   easing: "cubic-bezier(0.76, 0, 0.24, 1)",
-  duration: 500
+  duration: 500,
+  onChange: function() {
+    this.handleBottomNav();
+  }
 });
 
 // Controls
 const prevBtns = document.querySelectorAll(".prev");
 const nextBtns = document.querySelectorAll(".next");
 for (const prevBtn of prevBtns) {
-  prevBtn.addEventListener("click", () => mySiema.prev());
+  prevBtn.addEventListener("click", () => mySiemaInstance.prev());
 }
 for (const nextBtn of nextBtns) {
-  nextBtn.addEventListener("click", () => mySiema.next());
+  nextBtn.addEventListener("click", () => mySiemaInstance.next());
 }
